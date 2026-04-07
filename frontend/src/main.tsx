35d32f610233403bc15282629ad1ca5f7663d29e
jeevanvishnu <jeevanvishnu18@gmail.com>
2026-04-07 17:03:02 +0530

    feat: implement settings page with theme provider and profile management components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import { ThemeProvider } from './context/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider defaultTheme="light" storageKey="orchestrator-theme">
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
